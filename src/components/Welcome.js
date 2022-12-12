b1e15fb12e44052ea4ce1ff7f868d97c3f657709
pragatibhandari <bhandariprag7@gmail.com>
2022-12-12 15:50:33 +0200

    used json data for welcome

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,31 +1,18 @@
 import React from "react";
+import galleryImagesData from "../components/data/gallery_images.json";
 
 function Welcome() {
   return (
     <div className="scene" id="welcome">
       <article className="content">
         <div className="gallery">
-          <img
-            src="https://landonhotel.com/images/hotel/intro_room.jpg"
-            alt="Intro Gallery Room Sample Pictures"
-          />
-          <img
-            src="https://landonhotel.com/images/hotel/intro_pool.jpg"
-            alt="Intro Gallery Pool Sample Pictures"
-          />
-          <img
-            src="https://landonhotel.com/images/hotel/intro_dining.jpg"
-            alt="Intro Gallery Dining Sample Pictures"
-          />
-          <img
-            src="https://landonhotel.com/images/hotel/intro_attractions.jpg"
-            alt="Intro Gallery Attractions Sample Pictures"
-          />
-          <img
-            className="hidesm"
-            src="https://landonhotel.com/images/hotel/intro_wedding.jpg"
-            alt="Intro Gallery Dining Sample Pictures"
-          />
+          {galleryImagesData.map((images) => (
+            <img
+              src={images.src}
+              alt={images.alt}
+              className={images.className}
+            />
+          ))}
         </div>
         <h1>Welcome to the Yak and Yeti &nbsp;Hotel</h1>
         <p>
