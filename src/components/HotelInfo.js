8f9a8ed4184f1d3034788b32ba74a85874889e33
pragatibhandari <bhandariprag7@gmail.com>
2022-12-27 17:15:02 +0200

    banner image updated

--- src/components/HotelInfo.js
+++ src/components/HotelInfo.js
@@ -98,16 +98,16 @@ const HotelInfo = () => {
         </section>
       </article>
       <article id="greenprogram">
-        <h2>Landon Green Program</h2>
+        <h2>Yak and Yeti Green Program</h2>
         <p>
-          <strong>The Landon Hotel - London</strong> was recently renovated, and
-          we considered the impact on the earth the entire way. From green
-          building materials, to solar power, to energy-friendly lighting and
-          appliances throughout the hotel - we’re saving energy in every socket,
-          outlet, and switch. We’ve also initiated a recycling and composting
-          program that reduces the load to local landfills, while providing
-          valuable raw material for use in new products, or in the case of
-          compost, for use in local gardens and landscapes.
+          <strong>The Yak and Yeti Hotel - London</strong> was recently
+          renovated, and we considered the impact on the earth the entire way.
+          From green building materials, to solar power, to energy-friendly
+          lighting and appliances throughout the hotel - we’re saving energy in
+          every socket, outlet, and switch. We’ve also initiated a recycling and
+          composting program that reduces the load to local landfills, while
+          providing valuable raw material for use in new products, or in the
+          case of compost, for use in local gardens and landscapes.
         </p>
       </article>
     </div>
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -28,14 +28,14 @@ const Welcome = () => {
             <img className={image.className} src={image.src} alt={image.alt} />
           ))}
         </div>
-        <h1>Welcome to the Landon&nbsp;Hotel</h1>
+        <h1>Welcome to the Yak and Yeti&nbsp;Hotel</h1>
         <p>
-          The original Landon perseveres after 50 years in the heart of West
-          London. The West End neighborhood has something for everyone—from
+          The original Yak ans Yeti perseveres after 50 years in the heart of
+          Kathmandu. The West End neighborhood has something for everyone—from
           theater to dining to historic sights. And the not-to-miss Rooftop Cafe
           is a great place for travelers and locals to engage over drinks, food,
-          and good&nbsp;conversation. &nbsp;To learn more about the Landon Hotel
-          in the West End, browse our website and{" "}
+          and good&nbsp;conversation. &nbsp;To learn more about the Yak and Yeti
+          Hotel in the West End, browse our website and{" "}
           <a href="files/landon_information_sheet_London.pdf">
             download our handy information sheet
           </a>
